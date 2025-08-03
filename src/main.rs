b13b8a66ce3decb26f3f474c1d95e1fb4ae1d524
Clarice Dall'Orto <clarirad@clarices-mbp.mynet>
2025-08-03 17:12:16 +0100

    Add window customisation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins(DefaultPlugins) // game window
+        .add_plugins
+        (
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window { 
+                        resolution: WindowResolution::new(640., 480.).into(),
+                        title: "Game Name".into(),
+                        name: Some("Primary Window".to_string()),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+        ) // game window
         .insert_resource(ClearColor(Color::linear_rgb(0.5, 0., 0.2))) // backgorund colour
         .add_systems(Startup, setup) // spawn camera
         .run()
